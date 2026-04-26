768b5a303b7e86048a55396290eb05c52f784fa4
Xyrus John <xyvertucio@gmai.com>
2026-04-27 00:06:35 +0800

    modified: Add Project Placeholder on Project Section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { ProjectCard } from "@/components/ProjectCard";
 
 export const Projects = () => {
-  return <div>adsasda</div>;
+  return (
+    <section
+      id="projects"
+      className="flex items-center flex-col min-h-screen py-24 px-4 gap-8"
+    >
+      <h1 className="font-bold text-2xl">Projects</h1>
+      <p className="text-gray-500">
+        A selection of high-impact digital solutions, built with focus on
+        scalability, performance, and exceptional user experience.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </section>
+  );
 };
