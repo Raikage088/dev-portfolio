bf373f902bb87d89961d9d2401212887163583d3
Xyrus John <xyvertucio@gmai.com>
2026-04-28 00:59:12 +0800

    feat: Add Sysu Go driver app to projects list

--- src/data/project.js
+++ src/data/project.js
@@ -1,4 +1,5 @@
 import p1 from "../assets/projects/p1.png";
+import p2 from "../assets/projects/p2.png";
 
 export const createProject = ({
   id,
@@ -24,7 +25,7 @@ export const projects = [
     title: "Sysu Centralized Management System",
     image: p1,
     description:
-      "A full-stack application for SYSU International Inc. By integrating with SAP and adding a custom-built dashboard, with a central space to monitor the status, location and manage user data with real-time accuracy.",
+      "A full-stack web application for SYSU International Inc. By integrating with SAP and adding a custom-built dashboard, with a central space to monitor the status, location and manage user data with real-time accuracy.",
     technologies: [
       "HTML",
       "CSS",
@@ -33,6 +34,25 @@ export const projects = [
       "Express",
       "Socket.IO",
       "MySQL",
+      "Firebase",
+    ],
+    githubUrl: "",
+    liveUrl: "",
+  }),
+  createProject({
+    id: "p2",
+    title: "Sysu Go (Driver App)",
+    image: p2,
+    description:
+      "A real-time native application for drivers integrated for Sysu Centralized Management System. It features a custom-built dashboard to monitor vehicle status and precise GPS locations, leveraging a high-performance backend to ensure data accuracy and streamlined fleet operations.",
+    technologies: [
+      "Kotlin",
+      "JavaScript",
+      "Node.js",
+      "Express",
+      "Socket.IO",
+      "MySQL",
+      "Firebase",
     ],
     githubUrl: "",
     liveUrl: "",
